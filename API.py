068ddf4420d63811ea87158b4d79082f93c27b23
Agnès Regaud <agnes.regaud@yahoo.com>
2023-11-26 20:01:22 +0100

    Suppression imputer et scaler

--- API.py
+++ API.py
@@ -123,8 +123,8 @@ def data_customer():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host="0.0.0.0", port=8080, debug=True) #en local
+    app.run(debug=True)
+    #app.run(host="0.0.0.0", port=8080, debug=True) #en local
 
 
 
